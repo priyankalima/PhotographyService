4c587c5c510d37f098406617cd43951b177ff782
priyankalima <priyankalima99@gmail.com>
2024-11-04 14:34:11 +0530

    commit

---
+++ src/scripts/root.js
@@ -0,0 +1,11 @@
+const file = [
+    "./src/scripts/header.js",
+    "./src/scripts/main.js",
+    "./src/scripts/footer.js"
+]
+
+file.forEach((item)=>{
+    const script = document.createElement('script');
+    script.src = item;
+    document.body.appendChild(script);
+})
