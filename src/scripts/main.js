769bd819e248ba9a9680dbc90c246f2a58ccab15
priyankalima <priyankalima99@gmail.com>
2024-11-05 13:06:55 +0530

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,39 @@
 main.appendChild(
+    // banner section
     Object.assign(
         document.createElement('section'),
         {
             className:"banner",
             id:"banner",
+            innerHTML: `
+             <div class="carousel-content" id="carouselContent"></div>
+             <div class="carousel-control" id="carouselControl"></div>
+            `,
             function: addEventListener("load",()=>{
                 fetch('./content.json').then(res=>res.json()).then(data=>{
                     const item = data.main;
-                    console.log(item)
-                    banner.innerHTML = `
-                    <div class="container d-flex">
-                        <div class="left-content">
-                           <span class="title"> ${item.title}</span>
-                           <span class="para">${item.para}</span>
+                    item.forEach(e => {
+                        carouselContent.innerHTML += `
+                        <div class="carousel">
+                            <div class="container d-flex">
+                                <div class="left-content">
+                                    <span class="title"> ${e.title}</span>
+                                    <span class="para">${e.para}</span>
+                                </div>
+                                <div class="right-img">
+                                    <img src=${e.img}>
+                                </div>
+                            </div>
                         </div>
-                        <div class="right-img">
-                           <img src=${item.img}>
-                        </div>
-                    </div>
-                    `
+                        `
+                    });
+
+                    carouselContent.addEventListener('click',()=>{
+                        // alert('hello')
+                    })
+                    
+                    
+                    
                 })
             })
         }
